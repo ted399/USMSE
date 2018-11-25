9811288eb2b694fa415be63cf91d2ea4a147ebf8
Tadeas Hort <tadeashort@centrum.cz>
2018-11-25 18:01:43 +0100

    scrape offers into an object and print them

--- scraper.js
+++ scraper.js
@@ -1,10 +1,20 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://motorky.bazos.cz/skutry/');
-  await page.screenshot({path: 'example.png'});
+  await page.goto("https://motorky.bazos.cz/skutry/");
+
+  const offers = await page.evaluate(
+    () => Array.from(document.querySelectorAll("table.inzeraty"))
+      .map(offer => ({
+        title: offer.querySelector('span.nadpis > a').innerText.trim(),
+        image: offer.querySelector('img.obrazek').src,
+        price: offer.querySelector('span.cena > b').innerText.trim()
+      }))
+  );
+
+  console.log(offers);
 
   await browser.close();
 })();
